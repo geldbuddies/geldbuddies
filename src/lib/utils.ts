59f27cddb7b252785adbe0619e23bbcb68b0c3a3
gijsbartman <107071577+gijsbartman@users.noreply.github.com>
2025-03-31 14:06:24 +0200

    feat: add date formatting utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(month: number, year: number) {
+  const months = [
+    'Januari',
+    'Februari',
+    'Maart',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Augustus',
+    'September',
+    'Oktober',
+    'November',
+    'December',
+  ];
+
+  return `${months[month - 1]} ${year}`;
 }
