3a803e4f9faba09fc0e06a74ffc9530c50a987d2
gijsbartman <107071577+gijsbartman@users.noreply.github.com>
2025-03-31 22:45:24 +0200

    feat: add shadcn sidebar

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -3,8 +3,12 @@ import { Fragment } from 'react';
 export default function GameLayout({ children }: { children: React.ReactNode }) {
   return (
     <Fragment>
-      <header className="fixed">Game layout</header>
-      <main className="flex flex-col items-center justify-center h-screen">{children}</main>
+      <header className="fixed">Gamebuddies</header>
+      <aside>{/* Sidebar */}</aside>
+      <main className="flex h-screen">
+        <section>{children}</section>
+      </main>
+      <aside>{/* History */}</aside>
     </Fragment>
   );
 }
--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,27 +1,5 @@
 'use client';
 
-import { TimeDisplay } from '@/components/game/time-display';
-import { Button } from '@/components/ui/button';
-import { useGameStore } from '@/store/game-store';
-
 export default function GamePage() {
-  const { resetGame } = useGameStore();
-
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-        GeldBuddies Financial Simulator
-      </h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-        <TimeDisplay />
-      </div>
-
-      <div className="mt-8">
-        <Button variant="destructive" onClick={resetGame}>
-          Reset Game
-        </Button>
-      </div>
-    </div>
-  );
+  return <div className="container mx-auto p-4"></div>;
 }
