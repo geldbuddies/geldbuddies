7765838b4f30f52f67160a320823611efeefc3fa
gijsbartman <107071577+gijsbartman@users.noreply.github.com>
2025-04-15 08:13:19 +0200

    feat: add better-auth

---
+++ src/server/auth/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '../db'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, { provider: 'pg' }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    minPasswordLength: 8,
+    maxPasswordLength: 128,
+    passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    passwordRegexError:
+      'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    sendResetPassword: async ({ user, url }) => {
+      // TODO: Setup mail service to send reset password email
+      console.log('Reset password email sent to', user.email, 'with url', url)
+    },
+  },
+})
