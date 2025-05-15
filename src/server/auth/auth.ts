bcdc371a5ddda5634ebec61cf5e530845ca95be6
gijsbartman <107071577+gijsbartman@users.noreply.github.com>
2025-05-15 09:38:44 +0200

    feat: install better-auth organization plugin and update auth schema

--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,11 +1,11 @@
-import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { z } from 'zod';
+import { organization } from 'better-auth/plugins';
 import { db } from '../db';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: 'pg' }),
+  plugins: [organization()],
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
