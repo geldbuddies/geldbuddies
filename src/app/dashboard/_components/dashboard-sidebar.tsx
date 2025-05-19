e891afe2fad448d4eff8a87649d9c922d2f3f533
gijsbartman <107071577+gijsbartman@users.noreply.github.com>
2025-05-19 11:49:07 +0200

    feat: update DashboardSidebar to include new navigation link and icon

--- src/app/dashboard/_components/dashboard-sidebar.tsx
+++ src/app/dashboard/_components/dashboard-sidebar.tsx
@@ -1,4 +1,4 @@
-import { GalleryVerticalEnd } from 'lucide-react';
+import { ArrowUpLeft, GalleryVerticalEnd } from 'lucide-react';
 import * as React from 'react';
 
 import {
@@ -14,6 +14,7 @@ import {
   SidebarMenuSubItem,
   SidebarRail,
 } from '@/components/ui/sidebar';
+import Link from 'next/link';
 
 // This is sample data.
 const data = {
@@ -46,15 +47,14 @@ export function DashboardSidebar({ ...props }: React.ComponentProps<typeof Sideb
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="#">
+              <Link href="/">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
+                  <ArrowUpLeft className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
+                  <span className="font-semibold">Terug naar home</span>
                 </div>
-              </a>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
